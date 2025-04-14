e8553fc255cc2bedcee092e79cebc3c80a0d5d04
manuel griesbaum <manuel-griesbaum@gmx.de>
2025-04-14 16:16:59 +0200

    neue datei hinzufügen

---
+++ src/objects/GameObject.java
@@ -0,0 +1,7 @@
+package objects;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    public abstract void render(Graphics2D g, int tileSize);
+}
---
+++ src/objects/tiles/Tile.java
@@ -0,0 +1,13 @@
+package objects.tiles;
+
+import objects.GameObject;
+
+public abstract class Tile extends GameObject {
+    protected final int x;
+    protected final int y;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
