c41f233dcaac2be90ad7bb29383b6c0249f322cd
manuel griesbaum <manuel-griesbaum@gmx.de>
2025-04-16 14:10:30 +0200

    neue datei hinzufügen

--- src/game/Display.java
+++ src/game/Display.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Display extends JPanel {
-
     private final Game game;
 
     public Display(Game game) {
@@ -20,10 +19,6 @@ public class Display extends JPanel {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-
-        g.setColor(Color.RED);
-        g.drawRect(50, 100, 200, 200);
-        g.drawOval(200, 200, 100, 100);
 
         game.render(g2);
     }
--- src/game/objects/creatures/Player.java
+++ src/game/objects/creatures/Player.java
@@ -1,5 +1,10 @@
 package game.objects.creatures;
+
 import game.Game;
+import game.objects.creatures.enemy.Enemy;
+import game.objects.tiles.Air;
+import game.objects.tiles.Dot;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,15 +15,60 @@ public class Player extends Creature implements KeyListener {
         super(game, centerX, centerY, radius, speed, Color.YELLOW);
     }
 
+    private void tickDotCollision() {
+        int x = (int) centerX;
+        int y = (int) centerY;
+
+        if (game.getMap().getTile(x, y) instanceof Dot dot) {
+            double dx = dot.getCenterX() - centerX;
+            double dy = dot.getCenterY() - centerY;
+            double r = dot.getRadius() + radius;
+
+            if (dx * dx + dy * dy < r * r) {
+                game.getMap().setTile(x, y, new Air(x, y));
+                if (game.getMap().dotCount() == 0) {
+                    game.win();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void tickPreferredDirection() {
+        for (Enemy enemy : game.getEnemies()) {
+            enemy.tickPreferredDirection();
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        tickDotCollision();
+    }
+
     @Override
     public void render(Graphics2D g, int tileSize) {
         double centerXOnScreen = centerX * tileSize;
         double centerYOnScreen = centerY * tileSize;
         double radiusOnScreen = radius * tileSize;
-        double diameterOnScreen = radiusOnScreen * 2;
+        double diameterOnScreen = radiusOnScreen * 2.0;
 
         g.setColor(color);
         g.fill(new Ellipse2D.Double(centerXOnScreen - radiusOnScreen, centerYOnScreen - radiusOnScreen, diameterOnScreen, diameterOnScreen));
+
+        // Eyes
+        Enemy closestEnemy = null;
+        double closestSqDistance = Double.MAX_VALUE;
+        for (Enemy enemy : game.getEnemies()) {
+            double difX = enemy.centerX - centerX;
+            double difY = enemy.centerY - centerY;
+            double sqDistance = difX * difX + difY * difY;
+            if (sqDistance < closestSqDistance) {
+                closestEnemy = enemy;
+                closestSqDistance = sqDistance;
+            }
+        }
+        renderEyes(g, centerXOnScreen, centerYOnScreen, radiusOnScreen, closestEnemy.centerX, closestEnemy.centerY);
     }
 
     @Override
@@ -30,20 +80,20 @@ public class Player extends Creature implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W -> {
-                prefferedDirectionX = 0;
-                prefferedDirectionY = -1;
-            }
-            case KeyEvent.VK_S -> {
-                prefferedDirectionX = 0;
-                prefferedDirectionY = 1;
+                preferredDirectionX = 0;
+                preferredDirectionY = -1;
             }
             case KeyEvent.VK_A -> {
-                prefferedDirectionX = -1;
-                prefferedDirectionY = 0;
+                preferredDirectionX = -1;
+                preferredDirectionY = 0;
+            }
+            case KeyEvent.VK_S -> {
+                preferredDirectionX = 0;
+                preferredDirectionY = 1;
             }
             case KeyEvent.VK_D -> {
-                prefferedDirectionX = 1;
-                prefferedDirectionY = 0;
+                preferredDirectionX = 1;
+                preferredDirectionY = 0;
             }
         }
     }
